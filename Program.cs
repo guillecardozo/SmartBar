37dfc6667d00580f08f5dc08f86612b762987466
Guillermo Cardozo <GC195385@fi365.ort.edu.uy>
2025-03-26 11:17:40 -0300

    Implementando Onion Architecture

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using SmartBar.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 // Add services to the container.
 
@@ -8,10 +20,19 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
 
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- obj/Debug/net9.0/SmartBar.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/SmartBar.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartBar.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0419a0ffc40e9290fa5080a7755af72a1b4565fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartBar.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartBar.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
